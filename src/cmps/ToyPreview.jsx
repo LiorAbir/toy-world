3f92d71a59615d7466e5dec11e46c415f516d124
Lior Abir <liorabir909@gmail.com>
2022-11-24 15:19:47 +0200

    added inner scroll style in toy list

--- src/cmps/ToyPreview.jsx
+++ src/cmps/ToyPreview.jsx
@@ -1,13 +1,14 @@
 import { Link } from 'react-router-dom'
 
 export function ToyPreview({ toy, onRemoveToy }) {
-	const toyStyle = { backgroundImage: `url(https://robohash.org/${toy._id})` }
+	// const toyStyle = { backgroundImage: `url(https://robohash.org/${toy._id})` }
 	return (
-		<div className="toy-preview flex" style={toyStyle}>
-			<Link className="info" to={`/toy/${toy._id}`}>
-				<h2>{toy.name}</h2>
-				<h4>{toy.price}</h4>
-				<p>{toy.desc}</p>
+		<div className="toy-preview flex">
+			<Link className="info flex" to={`/toy/${toy._id}`}>
+				<img src={toy.img} alt="toy image" />
+				<h3>{toy.name}</h3>
+				<h4>Price:{toy.price}</h4>
+				{/* <p>{toy.desc}</p> */}
 			</Link>
 			<section className="actions flex">
 				<Link className="btn edit-btn" to={`/toy/edit/${toy._id}`}>
