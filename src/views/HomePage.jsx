bf8bd8d1007bd027d5a0768ef7b0cd1d2cff00ec
Lior Abir <liorabir909@gmail.com>
2022-12-05 18:05:05 +0200

    added home page

--- src/views/HomePage.jsx
+++ src/views/HomePage.jsx
@@ -1,10 +1,25 @@
 import { Component } from 'react'
+// import { ReactComponent as toyCar } from '../assets/imgs/toy-car.svg'
+import { ReactComponent as ToyCar } from '../assets/imgs/toy-car.svg'
 
 export class HomePage extends Component {
 	render() {
 		return (
 			<div className="home-page">
-				<h1>home page</h1>
+				<div className="hero flex">
+					<div className="hero-content">
+						<div className="text">
+							<h1>Toy World</h1>
+							<h3>
+								Lorem, ipsum dolor sit amet consectetur adipisicing
+								elit.
+							</h3>
+						</div>
+						<button className="btn start-btn">Start Shopping</button>
+					</div>
+					<div className="empty-div"></div>
+					{/* <ToyCar className="hero-img" /> */}
+				</div>
 			</div>
 		)
 	}
