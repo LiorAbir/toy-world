6be090416e21b3049c1b412b5bb6d8a38f4130cf
Lior Abir <liorabir909@gmail.com>
2023-01-04 10:00:53 +0200

    added no toys svg scss

--- src/cmps/ToyList.jsx
+++ src/cmps/ToyList.jsx
@@ -3,24 +3,26 @@ import { ReactComponent as TeddyBear } from '../assets/imgs/teddy-bear.svg'
 
 export function ToyList({ toys, user, onRemoveToy, onRemoveFromUser, onAddToUser }) {
 	return (
-		<div className="toy-list card-grid">
+		<>
 			{!toys || !toys.length ? (
 				<div className="no-toys flex">
 					<TeddyBear />
 					<h2>NO TOYS</h2>
 				</div>
 			) : (
-				toys.map((toy) => (
-					<ToyPreview
-						key={toy._id}
-						toy={toy}
-						user={user}
-						onRemoveToy={onRemoveToy}
-						onRemoveFromUser={onRemoveFromUser}
-						onAddToUser={onAddToUser}
-					/>
-				))
+				<div className="toy-list card-grid">
+					{toys.map((toy) => (
+						<ToyPreview
+							key={toy._id}
+							toy={toy}
+							user={user}
+							onRemoveToy={onRemoveToy}
+							onRemoveFromUser={onRemoveFromUser}
+							onAddToUser={onAddToUser}
+						/>
+					))}
+				</div>
 			)}
-		</div>
+		</>
 	)
 }
